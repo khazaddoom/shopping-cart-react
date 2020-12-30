478b844363cf68fcaf1c40c44941a1c148b250ea
khazaddoom <31121986g@gmail.com>
2020-12-30 18:48:06 +0530

    cart append product feature

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-function Cart() {
+function Cart({cartItems}) {
     return (
         <div>
-            Cart
+            { cartItems.map(item => <h1>{ item.cart_item_id + " : " + item.qty }</h1>) }
         </div>
     )
 }
