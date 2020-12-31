633d79f45b8639e6ae3ec4289815d3886d24d539
khazaddoom <31121986g@gmail.com>
2020-12-31 16:39:56 +0530

    minor styles added

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Product from './Product'
+import './Cart.css'
 let id = 1
 
 function Cart({cartItems, handleAddToCart, handleRemoveFromCart}) {
     console.log(cartItems)
     return (
-        <div>
+        <div className="single-item-grid">
             { cartItems.map(product => (<Product key={id++} {...product} handleRemoveFromCart={handleRemoveFromCart} handleAddToCart={handleAddToCart}/>))}
         </div>
     )
