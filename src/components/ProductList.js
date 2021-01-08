021f03bb985b398ec8f49bc0dbf0cada17cb22d7
khazaddoom <31121986g@gmail.com>
2021-01-08 17:30:32 +0530

    Added minor product card styling

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,19 +1,21 @@
 import React from 'react'
 import Product from './Product'
 import './ProductList.css'
+import Paper from '@material-ui/core/Paper'
+import Grid from '@material-ui/core/Grid'
 
 let id = 1
 
 const ProductList = ({products, cartItems}) => {
 
     return products.length == 0 ? <h1>No Products available for purchase at this moment!</h1> :
-    <div className="grid"> {
+    <Grid container spacing={2}> {
                 products.map(product => {
                     let found = cartItems.find(item => item.id == product.id)
                     product.qty = (found && found.qty) || 0
                     return product
                 })
-                .map(product => (<Product key={id++} {...product} />)) } </div>
+                .map(product => (<Grid item><Paper spacing={2}><Product key={id++} {...product} /></Paper></Grid>)) } </Grid>
 }
 
 export default ProductList
